1b1436d65db6816776f7e332a4683954b72c54ba
Gleb Shkut <gleb.shkut@gmail.com>
2022-03-14 17:56:54 -0700

    unit tests for User auth and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, :length => {:within => 8..Float::INFINITY}
+
+  
+  before_validation :change_to_lower_case
+
+
+  private
+    def change_to_lower_case
+      self.email.downcase!
+    end
+
+  def self.authenticate_with_credentials(email, password)
+    email.downcase!
+    user = User.find_by(email: email.strip)
+    p user
+    if user && user.authenticate(password)
+      return user
+    end
+  end
 end
